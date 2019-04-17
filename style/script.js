730b949b9e45e1b272f2a387b12ca637c334610e
Tatyana Molchanova <44355525+TatyanaMolchanova@users.noreply.github.com>
2019-04-17 09:20:58 +0300

    initial commit

---
+++ style/script.js
@@ -0,0 +1,6 @@
+var test = document.getElementById("test");
+
+test.addEventListener("mouseover", function( event)) {
+                      
+    var capitalizedLink = link[0].toUpperCase() + link.slice(1).toLowerCase();
+                      }
